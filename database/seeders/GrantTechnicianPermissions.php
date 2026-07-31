dc6cc12d9dc6906e1b58d3dd4fe95d87963f2830
Fag Humberto <seu_email@example.com>
2026-07-30 23:45:33 -0300

    fix: usar namespace correto de Spatie para Permission e Role

--- database/seeders/GrantTechnicianPermissions.php
+++ database/seeders/GrantTechnicianPermissions.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Permission;
-use App\Models\Role;
 use App\Models\Tenant;
 use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Seeder;
 
 class GrantTechnicianPermissions extends Seeder
