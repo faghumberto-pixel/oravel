3963bba77dc502961f41fb45143ad117dd4ab552
Fag Humberto <seu_email@example.com>
2026-08-31 16:53:19 -0300

    style: breadcrumb com background transparente

--- resources/views/components/oravel-breadcrumb.blade.php
+++ resources/views/components/oravel-breadcrumb.blade.php
@@ -5,7 +5,7 @@
 @endphp
 
 @if ($module && $title)
-    <div class="px-6 py-3 bg-gray-950 dark:bg-gray-950">
+    <div class="px-6 py-3">
         <p class="text-sm text-white font-medium">
             <span>{{ $module }}</span>
             <span class="mx-2 text-gray-400">→</span>
