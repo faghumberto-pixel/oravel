68ebf6b6a689dea14c6e5e82531671b676e53fdf
Fag Humberto <seu_email@example.com>
2026-09-03 23:08:44 -0300

    Revert "improve: adiciona filament:cache-components ao deploy script"
    This reverts commit f1252dd4139396f9d0ec2f2e04c9951ee426801d.

--- deploy.sh
+++ deploy.sh
@@ -79,8 +79,6 @@ fi
 echo "🧹 Limpando cache..."
 php artisan optimize:clear
 php artisan config:cache
-php artisan cache:clear
-php artisan filament:cache-components
 
 echo "✨ DEPLOY CONCLUÍDO COM SUCESSO!"
 '
