983a8c6e0a3727bc5b27584bd810b12729c4907e
Fag Humberto <seu_email@example.com>
2026-09-11 14:52:32 -0300

    remove: unused import

--- app/Providers/Filament/CentralPanelProvider.php
+++ app/Providers/Filament/CentralPanelProvider.php
@@ -8,7 +8,6 @@ use App\Filament\Central\Pages\DashboardVisitantes;
 use App\Filament\Central\Pages\FunilVendas;
 use App\Filament\Central\Pages\Kanban;
 use App\Filament\Central\Pages\Programacao;
-use App\Filament\Central\Resources\LandingPageLeadResource;
 use App\Filament\Central\Resources\PlanResource;
 use App\Filament\Central\Resources\SalesLeadResource\Widgets\InteractionChannelChart;
 use App\Filament\Central\Resources\SalesLeadResource\Widgets\InteractionChannelStats;
