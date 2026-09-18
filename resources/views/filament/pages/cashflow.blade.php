064ac342498b5e1bfd6f97efa110ffbcbbd37e94
Fag Humberto <seu_email@example.com>
2026-09-18 11:39:02 -0300

    fix: make charts equal width at 50% each
    Changed to explicit grid with col-span-1 for each chart widget,
    ensuring both occupy exactly 50% width on medium screens and up.

--- resources/views/filament/pages/cashflow.blade.php
+++ resources/views/filament/pages/cashflow.blade.php
@@ -5,10 +5,11 @@
         />
     </div>
 
-    <div class="grid gap-6 md:grid-cols-2">
-        <x-filament-widgets::widgets
-            :widgets="$this->getFooterWidgets()"
-            :columnSpan="6"
-        />
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+        @foreach ($this->getFooterWidgets() as $widget)
+            <div class="col-span-1">
+                @livewire($widget)
+            </div>
+        @endforeach
     </div>
 </x-filament-panels::page>
